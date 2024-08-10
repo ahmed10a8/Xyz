990a266316e30b68eee53905a2a4983be9bd7cc5
Xyz <65559136+ahmed10a8@users.noreply.github.com>
2024-08-10 13:16:31 +0400

    start.bat

--- start.bat
+++ start.bat
@@ -1,16 +1,16 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
-net config server /srvcomment:"Windows Server By LittleFox" > errormsg.txt 2>&1
+net config server /srvcomment:"Windows Server By admin" > errormsg.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > errormsg.txt 2>&1
-net user LittleFox 123123aa /add >nul
+net user admin 123123aa /add >nul
 net localgroup administrators LittleFox /add >nul
-net user LittleFox /active:yes >nul
+net user admin /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant LittleFox:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
+ICACLS C:\Windows\Temp /grant admin:F >nul
+ICACLS C:\Windows\installer /grant admin:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:8080/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
